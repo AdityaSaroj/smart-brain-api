ba7e8fcfd8d097e39960d36a44a6ddb959eb3093
Aditya Saroj <adityasaroj001@gmail.com>
2020-05-11 23:50:03 +0530

    Adding port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("App is running!");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is running on ${process.env.PORT}`);
 });
